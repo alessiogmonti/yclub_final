1e2ed6249a0122b77f6a492b423385e7cd9e0b49
goblins <alessiomonti97@gmail.com>
2022-09-20 15:42:50 +0200

    video fix

--- src/components/MediaContainer.tsx
+++ src/components/MediaContainer.tsx
@@ -1,20 +1,21 @@
+import Media from '../assets/yclub_mobile.mp4'
 import { Suspense, useRef, useEffect } from "react"
 
 const MediaContainer = () => {
-    const videoRef = useRef(undefined)
-    useEffect(() => {
-        videoRef.current.defaultMuted= true
-    })
+    // const videoRef = useRef(undefined)
+    // // useEffect(() => {
+    // //     videoRef.current.defaultMuted= true
+    // // })
     return(
     <Suspense fallback={null}>
             <video 
             style={{borderRadius:'50px'}}
-            ref={videoRef}
+            // ref={videoRef}
             muted
-            playsInline
+            // playsInline
             loop width={'99%'} height={'99%'}>
             <source
-                src={'/yclub_mobile.mp4'}
+                src={Media}
                 type="video/mp4"
             />
             Your browser does not support the video tag.
