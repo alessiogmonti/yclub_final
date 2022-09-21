ad918cf810c13cb1e3a2a7ad58c4a4603ef8a204
goblins <alessiomonti97@gmail.com>
2022-09-21 13:55:04 +0200

    video

--- src/components/Home/MediaContainer.jsx
+++ src/components/Home/MediaContainer.jsx
@@ -1,10 +1,10 @@
 // import MobileMedia from '../../assets/Home/Media/yclub_mobile.mp4'
 // import WebMedia from '../../assets/Home/Media/yclub_web.mp4'
 import { Suspense, useRef, useEffect, useState } from "react"
-import { Box, Show, Hide, useBreakpointValue } from '@chakra-ui/react'
+import { Box, Show, Hide, useBreakpointValue, AspectRatio } from '@chakra-ui/react'
 
-const mobilemedia = () => (<iframe src="https://iframe.mediadelivery.net/embed/61195/6480af9c-5292-4857-ad8f-72d7ae71a4ae?autoplay=true&loop=true&muted=true" loading="lazy" style="border: none; position: absolute; top: 0; height: 100%; width: 100%;" allow="accelerometer; gyroscope; autoplay; encrypted-media; picture-in-picture;" allowfullscreen="true"></iframe>)
-const webmedia = () => (<iframe src="https://iframe.mediadelivery.net/embed/61195/0cbedb30-9d95-4757-9527-643abfaf302d?autoplay=true&loop=true&muted=true" loading="lazy" style="border: none; position: absolute; top: 0; height: 100%; width: 100%;" allow="accelerometer; gyroscope; autoplay; encrypted-media; picture-in-picture;" allowfullscreen="true"></iframe>)
+const mobilemedia = <div style={{position: 'relative', paddingTop: '177.77777777777777%'}}><iframe src="https://iframe.mediadelivery.net/embed/61195/6480af9c-5292-4857-ad8f-72d7ae71a4ae?autoplay=true&loop=true&muted=true" loading="lazy" style={{border: "none", position: "absolute", top: 0, height: "100%", width: "100%", borderRadius:'50px'}} allow="accelerometer; gyroscope; autoplay; encrypted-media; picture-in-picture;" allowFullScreen="true"></iframe></div>
+const webmedia = <div style={{position: 'relative', paddingTop:' 56.25%'}}><iframe src="https://iframe.mediadelivery.net/embed/61195/0cbedb30-9d95-4757-9527-643abfaf302d?autoplay=true&loop=true&muted=true" loading="lazy" style={{border: "none", position: "absolute", top: 0, height: "100%", width: "100%", borderRadius:'50px'}} allow="accelerometer; gyroscope; autoplay; encrypted-media; picture-in-picture;" allowFullScreen="true"></iframe></div>
 
 // const addSource = ( src ) => {
 //     let video = document.querySelector('video');
@@ -35,7 +35,7 @@ const MediaContainer = () => {
                 />
                 Your browser does not support the video tag.
                 </video> */}
-                { size.width < 500 ? <mobilemedia/> : <webmedia/>}
+                { size.width < 500 ? mobilemedia : webmedia}
         </Box>
 )}
 
